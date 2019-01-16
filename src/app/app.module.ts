ab62da3753d94d1cda2ea05f021bc1515717b76d
Velican <duzgunce.vd@gmail.com>
2019-01-16 11:31:42 +0000

    creation of form and document viewer components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FormViewerComponent } from './form-viewer/form-viewer.component';
+import { DocumentViewerComponent } from './document-viewer/document-viewer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormViewerComponent,
+    DocumentViewerComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/form-viewer/form-viewer.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-form-viewer',
+  templateUrl: './form-viewer.component.html',
+  styleUrls: ['./form-viewer.component.scss']
+})
+export class FormViewerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
