7cdf8b47c98f0bbde28d9517b1ce7137843b7914
Velican <duzgunce.vd@gmail.com>
2019-01-18 13:09:49 +0000

    moved components to assembly folder

---
+++ src/app/assembly/assembly-viewer/assembly-viewer.component.ts
@@ -0,0 +1,7 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-assembly-viewer',
+  templateUrl: './assembly-viewer.component.html'
+})
+export class AssemblyViewerComponent {}
