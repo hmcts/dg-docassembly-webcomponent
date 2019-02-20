5f8da3b2397de47e6a57041160123d0573108407
benouaer <taleb.benouaer@hmcts.net>
2019-02-20 14:57:20 +0000

    add more control examples to the stub

--- backend-stubs.js
+++ backend-stubs.js
@@ -14,22 +14,68 @@ app.use((req, res, next) => {
 app.get("/ui-definition", (req, res) => {
   res.send([
     {
-      'key': 'firstName',
+      'key': 'exampleInput',
       'type': 'input',
       'hideExpression': null,
       'templateOptions': {
-        'label': 'First name',
+        'label': 'Example text input',
         'options': []
       },
       'fieldArray': null
     },
     {
-      'key': 'lastName',
-      'type': 'input',
+      'key': 'exampleSelect',
+      'type': 'select',
       'hideExpression': null,
       'templateOptions': {
-        'label': 'Last name',
-        'options': []
+        'label': 'Select the correct option',
+        'options': [
+          { value: 'option-1', label: 'Option 1'},
+          { value: 'option-2', label: 'Option 2'},
+          { value: 'option-3', label: 'Option 3'},
+          { value: 'option-4', label: 'Option 4'}
+        ]
+      },
+      'fieldArray': null
+    },
+    {
+      'key': 'exampleCheckbox',
+      'type': 'checkbox',
+      'hideExpression': null,
+      'templateOptions': {
+        'label': 'Tick all that apply',
+        'options': [
+          { value: 'option-1', label: 'Option 1'},
+          { value: 'option-2', label: 'Option 2'},
+          { value: 'option-3', label: 'Option 3'},
+          { value: 'option-4', label: 'Option 4'}
+        ]
+      },
+      'fieldArray': null
+    },
+    {
+      'key': 'exampleRadio',
+      'type': 'radio',
+      'hideExpression': null,
+      'templateOptions': {
+        'label': 'Select one of the options below',
+        'options': [
+          { value: 'option-1', label: 'Option 1'},
+          { value: 'option-2', label: 'Option 2'},
+          { value: 'option-3', label: 'Option 3'},
+          { value: 'option-4', label: 'Option 4'}
+        ]
+      },
+      'fieldArray': null
+    },
+    {
+      'key': 'exampleTextArea',
+      'type': 'textarea',
+      'hideExpression': null,
+      'templateOptions': {
+        'label': 'Example text area',
+        'cols': 5,
+        'rows': 5
       },
       'fieldArray': null
     }
