30a01116f04e76657f8cf9362fdb30c88b1185ec
pawelkaczmarekuk <pawel.kaczmarek@gmail.com>
2019-03-05 10:33:38 +0000

    e2e working

--- proxy.conf.js
+++ proxy.conf.js
@@ -3,7 +3,21 @@ const execSync = require('child_process').execSync;
 const idamToken = execSync('./bin/idamToken.sh').toString().trim();
 const s2s = execSync('./bin/s2s.sh').toString().trim();
 
+console.log(`Authorization: ${idamToken}`);
+console.log(`ServiceAuthorization: ${s2s}`);
+
 module.exports = [
+  {
+    context: [
+      '/documents'
+    ],
+    target: 'http://localhost:4603',
+    secure: false,
+    'bypass':  function (req, res, proxyOptions) {
+      req.headers['user-roles'] = 'caseworker';
+      req.headers['ServiceAuthorization'] = s2s;
+    }
+  },
   {
     context: [
       '/api'
